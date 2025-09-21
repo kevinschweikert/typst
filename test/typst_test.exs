c483723b71cbdf651dcd59ac61a7c4ff86b51a79
Herman verschooten <Herman@octarion.eu>
2025-09-21 07:26:13 +0200

    Test for returned result

--- test/typst_test.exs
+++ test/typst_test.exs
@@ -3,10 +3,13 @@ defmodule TypstTest do
 
   doctest Typst
 
-  test "smoke test" do
+  test "simple test" do
     assert "= Hello world" == Typst.render_to_string("= Hello <%= name %>", name: "world")
 
     {:ok, pdf} = Typst.render_to_pdf("= Hello <%= name %>", name: "world")
-    assert is_binary(pdf)
+    assert <<37, 80, 68, 70, 45, _rest::binary>> = pdf
+
+    {:ok, [png]} = Typst.render_to_png("= Hello <%= name %>", name: "world")
+    assert <<137, 80, 78, 71, 13, 10, 26, 10, _rest::binary>> = png
   end
 end
